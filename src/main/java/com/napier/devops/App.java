2f30811cfe8fdce4cddb13a14adcf5df88d38855
Ifthikar Aboobakker <145112250+ifthikar10@users.noreply.github.com>
2025-11-01 23:23:52 +0000

    Get Employee Added

--- src/main/java/com/napier/devops/App.java
+++ src/main/java/com/napier/devops/App.java
@@ -1,5 +1,6 @@
 package com.napier.devops;
 import java.sql.*;
+
 public class App
 {
     /**
@@ -20,6 +21,43 @@ public class App
 
         // Disconnect from database
         a.disconnect();
+    }
+
+    /**
+     * Get employee by ID from database
+     */
+    public Employee getEmployee(int ID)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT emp_no, first_name, last_name "
+                            + "FROM employees "
+                            + "WHERE emp_no = " + ID;
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+            if (rset.next())
+            {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("emp_no");
+                emp.first_name = rset.getString("first_name");
+                emp.last_name = rset.getString("last_name");
+                return emp;
+            }
+            else
+                return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
+        }
     }
 
     /**
